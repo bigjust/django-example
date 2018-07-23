c64d5e7307d6b998fdc3cae2ffc67980b91150d4
Justin Caratzas <bigjust@lambdaphil.es>
2018-07-23 14:05:45 -0400

    use sqlite for db

--- config/settings/common.py
+++ config/settings/common.py
@@ -100,7 +100,7 @@ MANAGERS = ADMINS
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
-    'default': env.db('DATABASE_URL', default='postgres:///django_example'),
+    'default': env.db('DATABASE_URL', default='sqlite:///django_example'),
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
